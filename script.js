400932b7910927fcfcbf8c8564932bd6686393d3
Rahmat Hafiz J <rahmat.hafiz.jayadiningrat@students.uin-suska.ac.id>
2023-01-23 22:50:36 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+};
+
+const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+        if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+};
+
+const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+};
+
+loadQuiz();
+
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "PHP",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Car SUV's Saiboats",
+        b: "Cascading Simple Sheets",
+        c: "Cascading Style Sheets",
+        d: "Central Style Sheets",
+        correct: "c",
+    },
+    {
+        question: "What does HTML stand for??",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicpters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1997",
+        d: "None of the above",
+        correct: "b",
+    },
+    {
+        question: "Anda jomblo ya?",
+        a: "Benar. Jomblo akut sejak lahir",
+        b: "Yes",
+        c: "Absolutely yes",
+        d: "Tidak",
+        correct: "a",
+    },
+];
+
+submitButton.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) score++;
+        currentQuiz++;
+        if (currentQuiz < quizData.length) loadQuiz();
+        else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="history.go(0)">Play Again</button>
+        `;
+        }
+    }
+});
